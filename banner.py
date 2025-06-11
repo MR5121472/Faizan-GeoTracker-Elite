dca12469ec4c0fb170f42f3b5b491cf0c1eae6fe
MR5121472 <177574924+MR5121472@users.noreply.github.com>
2025-06-11 22:39:14 +0500

    banner.py

--- banner.py
+++ banner.py
@@ -1,7 +1,7 @@
+from colorama import Fore, Style
+
 def banner():
-    print('''
-╔═════════════════════════════════════╗
-║     Faizan™ Stealth Tracker v1.0   ║
-║       Power | Privacy | Precision  ║
-╚═════════════════════════════════════╝
-''')
+    print(Fore.RED + Style.BRIGHT + "═" * 60)
+    print(Fore.CYAN + Style.BRIGHT + "║{:^58}║".format("Faizan™ Stealth Tracker v1.0"))
+    print(Fore.GREEN + "║{:^58}║".format("Power | Privacy | Precision"))
+    print(Fore.RED + "═" * 60 + Style.RESET_ALL)
